304ef254e4d112b4272d2da5973f51957cb70d82
kumpei <7120076@stu.hus.ac.jp>
2018-08-25 06:55:42 +0100

    エンティティクラス追加

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user_cd", type="string", length=255, unique=true)
+     */
+    private $userCd;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=true)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name_k", type="string", length=255)
+     */
+    private $lastNameK;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name_k", type="string", length=255)
+     */
+    private $firstNameK;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_at", type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="update_at", type="datetime", nullable=true)
+     */
+    private $updateAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userCd
+     *
+     * @param string $userCd
+     *
+     * @return User
+     */
+    public function setUserCd($userCd)
+    {
+        $this->userCd = $userCd;
+
+        return $this;
+    }
+
+    /**
+     * Get userCd
+     *
+     * @return string
+     */
+    public function getUserCd()
+    {
+        return $this->userCd;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastNameK
+     *
+     * @param string $lastNameK
+     *
+     * @return User
+     */
+    public function setLastNameK($lastNameK)
+    {
+        $this->lastNameK = $lastNameK;
+
+        return $this;
+    }
+
+    /**
+     * Get lastNameK
+     *
+     * @return string
+     */
+    public function getLastNameK()
+    {
+        return $this->lastNameK;
+    }
+
+    /**
+     * Set firstNameK
+     *
+     * @param string $firstNameK
+     *
+     * @return User
+     */
+    public function setFirstNameK($firstNameK)
+    {
+        $this->firstNameK = $firstNameK;
+
+        return $this;
+    }
+
+    /**
+     * Get firstNameK
+     *
+     * @return string
+     */
+    public function getFirstNameK()
+    {
+        return $this->firstNameK;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     *
+     * @return User
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return bool
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     *
+     * @return User
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set updateAt
+     *
+     * @param \DateTime $updateAt
+     *
+     * @return User
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updateAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdateAt()
+    {
+        return $this->updateAt;
+    }
+}
+
